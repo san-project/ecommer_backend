a634cbd374445172c426c73d11b9d70672718e40
Nikhil-Gududas <nikhil001.dev@gmail.com>
2023-02-26 10:32:46 +0530

    initial commit

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("HELLO")
+})
+
+app.listen(5000, () => {
+    console.log("listening at 5000");
+})
